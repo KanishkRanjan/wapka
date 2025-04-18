a09c4acc41440f8c72c0a5b11a27f7f5e769f453
Kanishk <kanishkranjan17@gmail.com>
2025-04-18 22:46:36 +0530

    Following video

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://verified-doberman-47.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  console.log("Hi from server")
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  );
+}
